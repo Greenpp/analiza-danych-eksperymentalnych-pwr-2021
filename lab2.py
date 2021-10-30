84297dc98991b3118703207cec15b3d370885418
Jakub Ciszek <jakub@ratingcaptain.com>
2021-10-30 20:11:50 +0200

    Lab2 except fourier

--- lab2.py
+++ lab2.py
@@ -1,10 +1,18 @@
 # %%
+import warnings
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import linregress
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+warnings.filterwarnings("ignore")
 
 DATA_DIR = Path('./data')
 EXPORT_DIR = Path('./export_lab2')
@@ -30,15 +38,21 @@ def export(df: pd.DataFrame, name: str) -> None:
 
     name = name.split('.')[0]
 
+    print('Exporting to json')
     df.to_json(EXPORT_DIR / f'{name}.json')
+    print('Exporting to xml')
     df.to_xml(EXPORT_DIR / f'{name}.xml')
+    print('Exporting to csv')
     df.to_csv(EXPORT_DIR / f'{name}.csv')
+    print('Exporting to xslx')
     df.to_excel(EXPORT_DIR / f'{name}.xslx')
 
 
 def describe_data(df: pd.DataFrame, name: str):
-    print(f'File {name}')
-    print(df.describe().loc[['25%', '50%', '75%']])
+    desc = df.describe()
+    print(f'1 kwartyl:\n{desc.loc["25%"]}')
+    print(f'mediana:\n{desc.loc["50%"]}')
+    print(f'3 kwartyl:\n{desc.loc["75%"]}')
 
 
 def drop_outliers(df: pd.DataFrame) -> pd.DataFrame:
@@ -54,33 +68,108 @@ def drop_outliers(df: pd.DataFrame) -> pd.DataFrame:
 
 def r2_compare(df: pd.DataFrame) -> None:
     res = linregress(x=df['x'], y=df['y'])
-    print(f'R2 before: {res.rvalue ** 2}')
+    print(f'R2 przed odrzuceniem odstających wartości: {res.rvalue ** 2}')
     df_clean = drop_outliers(df)
-    res_clean = linregress(x=df['x'], y=df['y'])
-    print(f'R2 after: {res_clean.rvalue ** 2}')
+    res_clean = linregress(x=df_clean['x'], y=df_clean['y'])
+    print(f'R2 po odrzuceniu odstających wartości: {res_clean.rvalue ** 2}')
 
 
-def generate_data() -> np.ndarray:
+def generate_1d_data(plot: bool = False) -> np.ndarray:
     x = np.linspace(0, 5, 100)
     mu = 0
     sigma = 0.1
     noise = mu + sigma * np.random.randn(100)
     y = np.log(x + 1) + noise
 
+    if plot:
+        sns.scatterplot(x=x, y=y)
+        plt.show()
+        plt.clf()
+
     return y
+
+
+def generate_2d_data(plot: bool = False) -> np.ndarray:
+    x = np.linspace(0, 5, 100)
+    y = np.linspace(0, 5, 100)
+
+    z = np.sin(np.log(x + 2) * np.sqrt(y))
+
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('f')
+
+        ax.scatter(x, y, z)
+        plt.show()
+        plt.clf()
+
+    return z
+
+
+def calculate_coeff(x: np.ndarray, y: np.ndarray, degree: int) -> np.ndarray:
+    p = Pipeline(
+        [
+            ('poly', PolynomialFeatures(degree)),
+            ('lr', LinearRegression(fit_intercept=False)),
+        ]
+    )
+
+    p.fit(x, y)
+    coeff = p.named_steps["lr"].coef_
+    print(f'Współczynniki: {coeff}')
+
+    return coeff
+
+
+def get_design_matrix(data: np.ndarray, degree: int) -> None:
+    if len(data.shape) == 1:
+        data = data.reshape((-1, 1))
+
+    poly = PolynomialFeatures(degree)
+    m = poly.fit_transform(data)
+
+    print(m)
+
+
+def plot_poly(data: np.ndarray, coeff: np.ndarray) -> None:
+    degree = len(coeff) - 1
+
+    l = np.linspace(0, 5, 100)
+    y = sum([l ** i * n for i, n in enumerate(coeff)])
+
+    sns.scatterplot(x=l, y=data)
+    sns.lineplot(x=l, y=y, color='orange')
+    plt.show()
+    plt.clf()
 
 
 # %%
 for f_name in FILES:
-    df = load_data_lab1(f_name)
     print(50 * '=')
+    print(f'FILE: {f_name}')
+    df = load_data_lab1(f_name)
     print(df)
     export(df, f_name)
     describe_data(df, f_name)
     r2_compare(df)
-# %%
-y = generate_data()
-# %%
-sns.scatterplot(x=np.linspace(0, 5, 100), y=y)
 
+
+print('FUNKCJA 1 ZMIENNEJ')
+data_1d = generate_1d_data(plot=True)
+l = np.linspace(0, 5, 100).reshape((-1, 1))
+
+print('WIELOMIAN 1 STOPNIA')
+coeff_1 = calculate_coeff(l, data_1d, 1)
+plot_poly(data_1d, coeff_1)
+print('WIELOMIAN 3 STOPNIA')
+coeff_3 = calculate_coeff(l, data_1d, 3)
+plot_poly(data_1d, coeff_3)
+
+
+print('FUNKCJA 2 ZMIENNYCH')
+data_2d = generate_2d_data(plot=True)
 # %%
